08d6296190bc13e773d11d37719313165b14baf8
Strongeric89 <ericstrong89@gmail.com>
2016-09-12 19:45:50 +0100

    first git test

---
+++ PersonClass/__init__.py
@@ -0,0 +1,10 @@
+class Person():
+    def greeting(self):
+        print("Hello")
+
+p1 = Person()
+p1.greeting()
+
+name = 'eric strong'
+age = 27
+print("Hello my name is ",name, "and i am ", age)
