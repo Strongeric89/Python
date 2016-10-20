a224e408c246eab3a8b43a13360cae73c5ab00f0
Strongeric89 <ericstrong89@gmail.com>
2016-10-20 14:09:20 +0100

    week6: shop list

--- week6/tutorial.py
+++ week6/tutorial.py
@@ -1,45 +1,52 @@
 #the folowing program is used as a shopping app
 """the functionality of the app is to add, remove, check price, check description """
 
-#add item to the dictionary - description and price pair
-#
-# def goToAdd():
-#     option = raw_input("would you like to add items?[Y/N]")
-#     if option == 'Y' or 'y':
-#         addItem(shopItems)
-#         return 0
-#     elif option == 'n' or 'N':
-#         return 0
-#     else:
-#         print("not a valid option")
-#         return 0
-
 def checkLen(shopItems):
     if len(shopItems) < 1:
         print("There are no Items in the Shop List!")
+        return False
+    else:
+        return True
 
 
 #check price function
 def checkPrice(shopItems):
-    checkLen(shopItems)
-    item = raw_input("what item to get price on?")
-    item = item.lower()
 
-    #find the item
-    euro = '\xe2\x82\xac'
-    for key,value in shopItems.items():
-        if item == key :
-            print("the price of {} is {}{:.1f}".format(item, euro,value))
-            break
+    check =checkLen(shopItems)
+    if check == True:
+
+        item = raw_input("what item to get price on?")
+        item = item.lower()
+        print("please enter an item, not a value.")
+
+
+        #find the item
+        euro = '\xe2\x82\xac'
+        for key,value in shopItems.items():
+            if item == key :
+                print("the price of {} is {}{:.2f}".format(item, euro,value))
+                break
+        else:
+            print("The item %s is not in the shop list" %item)
     else:
-        print("The item %s is not in the shop list" %item)
+        print("")
 
-def addItem(shopItems):
+def addItem(shopItems, total):
     euro = '\xe2\x82\xac'
+    isRunning1=True
+
     itemToAdd = raw_input("enter Item Description: ")
     itemToAdd = itemToAdd.lower()
-    itemPrice = float(raw_input("enter Item Price:" + euro + " "))
 
+    while(isRunning1):
+        try:
+            itemPrice = float(raw_input("enter Item Price:" + euro + " "))
+            break
+        except ValueError:
+            print("please enter Value only. {:}0.00".format(euro))
+            isRunning1=True
+
+    total = total + itemPrice
     # check if the item already exists
     for key, value in shopItems.items():
         if itemToAdd == key:
@@ -47,38 +54,49 @@ def addItem(shopItems):
             break
     else:
         shopItems.update({itemToAdd: itemPrice})
-        print("%s added to the list" % (itemToAdd))
+        print("\n%s added to the list" % (itemToAdd))
 
-    return shopItems
+    return total
 
 #check price function
-def delItem(shopItems):
-    checkLen(shopItems)
-    item = raw_input("what item to delete?")
-    item = item.lower()
-    #find the item
-    for key,value in shopItems.items():
-        if item == key :
-            shopItems.pop(item)
-            print("{} was removed from the list".format(item))
-            break
+def delItem(shopItems,total):
+    check =checkLen(shopItems)
+    if check == True:
+        item = raw_input("what item to delete?")
+        item = item.lower()
+        #find the item
+        for key,value in shopItems.items():
+            if item == key :
+                shopItems.pop(item)
+                total = total - value
+                print("{} was removed from the list".format(item))
+                return total
+                break
+        else:
+            print("The item %s is not in the shop list" %item)
     else:
-        print("The item %s is not in the shop list" %item)
+        print("")
 
 #printing the entire dictionary
 def displayAll(shopItems):
-    checkLen(shopItems)
-    #display the entire dictionary
-    print("Price:\tDescritpion:")
-    print("*"*30)
-    euro = '\xe2\x82\xac'
+    check=checkLen(shopItems)
+    if check == True:
 
-    for key,value in shopItems.items():
-        print("{} {:.2f} | \t {}".format(euro,value,key))
+        #display the entire dictionary
+        print("Price:\tDescritpion:")
+        print("*"*30)
+        euro = '\xe2\x82\xac'
+
+        for key,value in shopItems.items():
+            print("{} {:.2f} | \t {}".format(euro,value,key))
+    else:
+        print(" ")
+
 
 #main
 
 #global dictionary
+total = 0.00
 shopItems = {}
 euro = '\xe2\x82\xac'
 
@@ -92,7 +110,7 @@ while(isRunning):
         #print("\n" * 3)
         print("*" * 30)
         try:
-            option = int(raw_input("Please select an option:\n[1]Add item to Shop\n[2]Remove Item from Shop\n[3]Check Price on Item\n[4]Get Display all products\n\nUSER: "))
+            option = int(raw_input("Please select an option:\n[1]Add item to Shop\n[2]Remove Item from Shop\n[3]Check Price on Item\n[4]Get Display all products\n[5]Display Total\n\nUSER: "))
             print("\n"*5)
             break
         except ValueError:
@@ -101,10 +119,10 @@ while(isRunning):
 
     if(option == 1):
         print("Add Item>>")
-        addItem(shopItems)
+        total = addItem(shopItems,total)
     elif (option == 2):
         print("Remove Item>>")
-        delItem(shopItems)
+        total = delItem(shopItems,total)
     elif (option == 3):
         print("check price on item>>")
         checkPrice(shopItems)
@@ -113,6 +131,9 @@ while(isRunning):
         displayAll(shopItems)
     elif(option == 0):
         print("exiting>>")
+        print("Thank you for using the Shopping List app. Goodbye")
         isRunning = False
+    elif(option ==5):
+        print("Your total Basket is {}{:.2f}".format(euro,total))
     else:
         print("not a valid option. Try again!")
